a982ed6e9c41fa9935665e7ca7b59c943fe81a31
Michael <wtfdic@hotmail.com>
2020-11-23 19:34:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return array.join(" and ")
+  else
+    return arrat[0..-2].join(', ') + ", and " + arrazy[-1]
+  end
 
 end
